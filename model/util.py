35f99cb4c59e9638e05b7494ffd4ab8c605493b2
PGockell <maxi.gockell@gmail.com>
2021-04-07 21:57:02 +0200

    Delete util.py
    Falsch

--- model/util.py
+++
@@ -1,9 +0,0 @@
-import joblib
-from matplotlib import pyplot as plt
-
-# quick plotting of posted image data
-
-image = joblib.load('
-plt.imshow(image, interpolation='none',
-           cmap=plt.cm.get_cmap("Greys"))
-plt.show()
